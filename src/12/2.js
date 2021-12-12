d3d38ddaf8f2acf11edd9dc25009f64c5f5700b0
IdeFFiX <ideffix1337@gmail.com>
2021-12-12 13:31:03 +0100

    day12

---
+++ src/12/2.js
@@ -0,0 +1,47 @@
+const { readInput } = require("../file");
+const edges = readInput().map(line => line.split('-'))
+
+const edgesMap = createGraph(edges)
+
+const result = traverse(edgesMap.get('start'))
+
+console.log(result)
+
+function createGraph(edges) {
+  const edgesMap = new Map();
+
+  for (const [from, to] of edges) {
+    const fromVertex = edgesMap.get(from) ?? createVertex(from)
+    const toVertex = edgesMap.get(to) ?? createVertex(to)
+
+    fromVertex.edges.push(toVertex)
+    toVertex.edges.push(fromVertex)
+
+    edgesMap.set(from, fromVertex)
+    edgesMap.set(to, toVertex)
+  }
+
+  return edgesMap
+}
+
+function createVertex(name) {
+  return {
+    name,
+    edges: [],
+    isSmallCave: name[0].toLowerCase() === name[0]
+  }
+}
+
+function traverse(start) {
+  return start.edges.reduce((acc, e) => acc + numberOfPathsToEnd(e, new Set([start]), false), 0)
+}
+
+function numberOfPathsToEnd(vertex, visited, isSingleSmallCaveVisitedTwice) {
+  if (vertex.name === 'end') return 1;
+  if (vertex.name === 'start' || (isSingleSmallCaveVisitedTwice && vertex.isSmallCave && visited.has(vertex))) return 0;
+
+  return vertex.edges.reduce((acc, e) => {
+    const twice = isSingleSmallCaveVisitedTwice || vertex.isSmallCave && visited.has(vertex)
+    return acc + numberOfPathsToEnd(e, new Set([...visited, vertex]), twice)
+  }, 0)
+}
